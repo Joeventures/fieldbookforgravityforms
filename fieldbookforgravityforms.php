35f0207bdd968d7ee05539c36e44b07f3b6090f4
Joe Winter <joe@joeventures.com>
2017-04-30 10:07:14 -0400

    Initial commit - include phieldbook

---
+++ fieldbookforgravityforms.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Fieldbook for Gravity Forms
+Description: Send Gravity Form submissions to FieldBook.
+Version: 0.1
+Author: Joe Winter
+Author URI: https://joe.ventures
+License: GPL2
+*/
+
+require_once dirname(__FILE__) . '/phieldbook.php';
